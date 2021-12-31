53ea248ce3a96558ccc17104130e334c53e5f942
tushar-josh <tushar.gupta@joshtechnologygroup.com>
2021-12-31 10:53:06 +0530

    third commit

--- tests/test_google.py
+++ tests/test_google.py
@@ -4,6 +4,6 @@ class HomeTest(BaseCase):
     def test_home_page(self):
         self.open("https://www.google.com/")
         self.assert_title("Google")
-    def test_home_page(self):
+    def test_home_page2(self):
         self.open("https://www.google.com/")
         self.assert_title("Googleee")
